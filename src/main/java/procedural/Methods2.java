7bb6ec72199d494450b61fe0ea7cb1862717aeac
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-10-03 13:27:04 +0200

    fields

--- src/main/java/procedural/Methods2.java
+++ src/main/java/procedural/Methods2.java
@@ -4,22 +4,23 @@ import java.util.Random;
 
 public class Methods2 {
 
-  // static int points; //pola maja domyslna wartosc tak jak puste tablice, maja modyfikatory dostepu
+  private static int points; //pola maja domyslna wartosc tak jak puste tablice, maja modyfikatory dostepu
+
 
     public static void main(String[] args) {
-        int points = 0;
+       // int points = 0; // zmienna lokalna (w metodzie) może przysłaniać pole (zmienną w klasie)
         for (int i = 0; i < 10 && points < 50; i++) {
-           points = rozegrajRunde(points);
+            playRound();
         }
-        System.out.println("Na koniec: " + points);
+        System.out.println("Na koniec: " +points);
     }
 
-    static int rozegrajRunde(int allPoints) {
+   private static void playRound() {
         Random random = new Random();
         int newPoints = random.nextInt(10) + 1; //1-10
-        allPoints += newPoints;
+        points += newPoints;
         System.out.println("Gracz dostaje: " + newPoints + " punktów");
-        System.out.println("Gracz ma: " + allPoints + " punktów łącznie");
-        return allPoints;
+        System.out.println("Gracz ma: " + points + " punktów łącznie");
     }
+
 }

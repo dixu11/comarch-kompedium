7d0e66c3fd8c69231fd58aed0de4b23f7a59cc22
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-10-03 13:37:08 +0200

    overloading - przeciążanie

---
+++ src/main/java/procedural/Overloading.java
@@ -0,0 +1,29 @@
+package procedural;
+
+import java.util.Arrays;
+
+public class Overloading {
+
+    public static void main(String[] args) {
+        System.out.println(add(1,2));
+        System.out.println(add(1.,2));
+        System.out.println(add(1.0,2));
+        System.out.println(add(23,45,34));
+
+    }
+
+    private static double add(int number1, int number2) {
+        System.out.println("M1");
+        return number1 + number2;
+    }
+
+    private static double add(double number1, double number2) {
+        System.out.println("M2");
+        return number1 + number2;
+    }
+
+    private static int add(int number1, int number2, int number3) {
+        System.out.println("M3");
+        return number1 + number2 + number3;
+    }
+}
